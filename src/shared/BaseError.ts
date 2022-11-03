2c6bedfa22d5ece6a2209b5911317d203ed8b6fc
Renan Batista <renfbatista@gmail.com>
2022-11-03 19:09:07 -0300

    update shared

--- src/shared/BaseError.ts
+++ src/shared/BaseError.ts
@@ -1,7 +1,11 @@
 export default class BaseError extends Error {
   code: string;
+  details?: string;
   httpCode: string;
-  constructor(code, message, httpCode?) {
+  constructor(code, message, details?, httpCode?) {
     super(message);
+    this.code = code;
+    this.httpCode = httpCode || code;
+    this.details = details;
   }
 }
